e008a29a7382a1372bf62ed140dc4b10b82a9ec6
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-18 09:51:06 +0700

    Update Human-Traffic-of-Stadium.sql

--- Human-Traffic-of-Stadium.sql
+++ Human-Traffic-of-Stadium.sql
@@ -39,9 +39,9 @@ WHERE t1.people >= 100 AND t2.people >= 100 AND t3.people >= 100
 AND
 (
     (t1.id - t2.id = 1 AND t1.id - t3.id = 2 AND t2.id - t3.id =1)  -- t1, t2, t3
-    OR
+ OR
     (t2.id - t1.id = 1 AND t2.id - t3.id = 2 AND t1.id - t3.id =1)  -- t2, t1, t3
-    OR
+ OR
     (t3.id - t2.id = 1 AND t2.id - t1.id = 1 AND t3.id - t1.id = 2) -- t3, t2, t1
 )
 ORDER BY t1.id;
