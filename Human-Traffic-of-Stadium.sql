9878a7e913155ed6fd0896c8335e7d2740317fab
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-18 09:50:48 +0700

    Update Human-Traffic-of-Stadium.sql

--- Human-Traffic-of-Stadium.sql
+++ Human-Traffic-of-Stadium.sql
@@ -40,7 +40,7 @@ AND
 (
     (t1.id - t2.id = 1 AND t1.id - t3.id = 2 AND t2.id - t3.id =1)  -- t1, t2, t3
     OR
-    (t2.id - t1.id = 1 AND t2.id - t3.id = 2 AND t1.id - t3.id =1) -- t2, t1, t3
+    (t2.id - t1.id = 1 AND t2.id - t3.id = 2 AND t1.id - t3.id =1)  -- t2, t1, t3
     OR
     (t3.id - t2.id = 1 AND t2.id - t1.id = 1 AND t3.id - t1.id = 2) -- t3, t2, t1
 )
