8ce7f03fa95caa596fb8f05cb16eea87b25ee43b
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-18 09:06:09 +0700

    Update Customers-Who-Never-Order.sql

--- Customers-Who-Never-Order.sql
+++ Customers-Who-Never-Order.sql
@@ -1,13 +1,21 @@
 --MySQL & MS SQL Server
-# Method 1:
+---Method 1:
 SELECT name AS Customers 
 FROM Customers
 WHERE id NOT IN (SELECT customerId  
                  FROM Orders);
                  
-# Method 2:
+---Method 2:
 SELECT name AS Customers
 FROM Customers c
 LEFT JOIN Orders o 
 ON c.id = o.customerId
 WHERE o.CustomerId IS NULL;
+
+---Method 3:
+SELECT name AS Customers 
+FROM Customers c
+WHERE NOT EXISTS (
+    SELECT customerId 
+    FROM Orders o
+    WHERE c.id = o.customerId);
