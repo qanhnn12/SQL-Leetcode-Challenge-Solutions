0d0ca024e195faa9b199cddf8eade9b6d67226df
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-19 20:05:47 +0700

    Create 1873.Calculate-Special-Bonus.sql

---
+++ Easy/1873.Calculate-Special-Bonus.sql
@@ -0,0 +1,7 @@
+--MySQL & MS SQL Server
+SELECT
+    employee_id,
+    CASE WHEN employee_id % 2 !=0 AND name NOT LIKE 'M%' THEN salary
+         ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id;
