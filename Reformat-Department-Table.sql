5c7d8f6dab77fd8cf88b2e1ec73007b51a7854d2
Anh Nguyen <84619797+qanhnn12@users.noreply.github.com>
2022-10-19 09:44:20 +0700

    Create Reformat-Department-Table.sql

---
+++ Reformat-Department-Table.sql
@@ -0,0 +1,40 @@
+--MySQL
+SELECT 
+    id,
+    SUM(CASE WHEN month = 'Jan' THEN revenue ELSE null END) AS Jan_Revenue,
+    SUM(CASE WHEN month = 'Feb' THEN revenue ELSE null END) AS Feb_Revenue,
+    SUM(CASE WHEN month = 'Mar' THEN revenue ELSE null END) AS Mar_Revenue,
+    SUM(CASE WHEN month = 'Apr' THEN revenue ELSE null END) AS Apr_Revenue,
+    SUM(CASE WHEN month = 'May' THEN revenue ELSE null END) AS May_Revenue,
+    SUM(CASE WHEN month = 'Jun' THEN revenue ELSE null END) AS Jun_Revenue,
+    SUM(CASE WHEN month = 'Jul' THEN revenue ELSE null END) AS Jul_Revenue,
+    SUM(CASE WHEN month = 'Aug' THEN revenue ELSE null END) AS Aug_Revenue,
+    SUM(CASE WHEN month = 'Sep' THEN revenue ELSE null END) AS Sep_Revenue,
+    SUM(CASE WHEN month = 'Oct' THEN revenue ELSE null END) AS Oct_Revenue,
+    SUM(CASE WHEN month = 'Nov' THEN revenue ELSE null END) AS Nov_Revenue,
+    SUM(CASE WHEN month = 'Dec' THEN revenue ELSE null END) AS Dec_Revenue
+FROM Department
+GROUP BY id;
+
+--MS SQL Server
+SELECT * FROM 
+    (SELECT 
+        revenue, month+'_Revenue' AS m, 
+        id 
+     FROM Department) t
+PIVOT (
+    SUM(revenue) 
+    FOR m IN (  [Jan_Revenue],
+                [Feb_Revenue],
+                [Mar_Revenue],
+                [Apr_Revenue],
+                [May_Revenue],
+                [Jun_Revenue],
+                [Jul_Revenue],
+                [Aug_Revenue],
+                [Sep_Revenue],
+                [Oct_Revenue],
+                [Nov_Revenue],
+                [Dec_Revenue]
+            )
+) AS pivot_table;
